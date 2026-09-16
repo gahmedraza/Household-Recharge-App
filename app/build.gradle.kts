d2730ad5d956373ca70e2b27e3db4af57415b37e
Raza <gulamahmed.raza110@gmail.com>
2026-09-17 01:08:22 +0530

    version upgraded in the gradle to aplha.1: first developer build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0"
+        versionName = "v1.0.0-alpha.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
